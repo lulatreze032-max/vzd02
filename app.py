f63ce753db6f83ef30bce9d3a40199576fe39f9d
lulatreze032-max <lulatreze032@gmail.com>
2026-02-09 03:06:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -184,6 +184,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # 🔒 START VISUAL
     try:
         await update.message.reply_video(video=START_VIDEO_URL)
+        
+        await update.message.reply_text(
+            MAIN_TEXT,
+            reply_markup=InlineKeyboardMarkup(keyboard)
+        )
 
         await context.bot.send_media_group(
             chat_id=update.effective_chat.id,
@@ -194,7 +199,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         logger.error(f"Erro ao enviar vídeos do start: {e}")
 
     # texto SEMPRE aparece
-    await update.message.reply_text(MAIN_TEXT, reply_markup=keyboard)
+    #await update.message.reply_text(MAIN_TEXT, reply_markup=keyboard)
 
     counter_msg = await update.message.reply_text(
         f"🔥🔞 *Membros Atuais 👥⬆:* {counter_value:,}".replace(",", "."),
